c88994a958ac3c71b39a4ba6de88290c0e6ebaf5
abzaidi <abubakar.zaidi03@gmail.com>
2025-08-11 00:34:50 +0500

    made changes

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ["**/*.PNG"]
+  assetsInclude: ["**/*.PNG"],
+  base: '/zaidi-visions/',
 }));
